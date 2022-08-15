adc0091c4371d1bcb78a4d4e47a04f789d9211eb
giorgi <gjanashia@unisens.ge>
2022-08-15 17:35:41 +0400

    initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# Football Stats
+
+It's a simple app for football statistic in 2018.
+
+## Installation
+
+Install the dependencies and devDependencies and start the server.
+
+```sh
+npm i
+npm start
+```
---
+++ src/index.ts
@@ -0,0 +1,9 @@
+import { Summary } from './Summary';
+import { MatchReader } from './MatchReader';
+
+const matchReader = MatchReader.fromCsv('football.csv');
+matchReader.load();
+
+const summary = Summary.winsAnalysisWithHtmlReport('Man United');
+
+summary.buildAndPrintReport(matchReader.matches);
